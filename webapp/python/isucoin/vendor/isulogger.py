4b7198c5ee892190dae619e27ad191353216a554
wass80 <wass80>
2018-10-20 13:11:45 +0900

    Async Log

--- webapp/python/isucoin/vendor/isulogger.py
+++ webapp/python/isucoin/vendor/isulogger.py
@@ -9,21 +9,29 @@ import urllib.parse
 
 import requests
 
+LOG_QUEUE_SIZE = 100
 
 class IsuLogger:
     def __init__(self, endpoint, appID):
         self.endpoint = endpoint
         self.appID = appID
+        self.queue = []
 
     def send(self, tag, data):
+        self.queue.append({
+            "tag": tag,
+            "time": time.strftime("%Y-%m-%dT%H:%M:%S+09:00"),
+            "data": data,
+        })
+        if len(self.queue) >= LOG_QUEUE_SIZE:
+            self.send_bulk
+    
+    def send_bulk(self):
         self._request(
-            "/send",
-            {
-                "tag": tag,
-                "time": time.strftime("%Y-%m-%dT%H:%M:%S+09:00"),
-                "data": data,
-            },
+            "/send_bulk", self.queue
         )
+        self.queue = []
+        
 
     def _request(self, path, data):
         url = urllib.parse.urljoin(self.endpoint, path)
